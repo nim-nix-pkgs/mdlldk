ec2fbc7e7a572de88e35d796f609061520e87995
hugosenari <hugosenari@gmail.com>
2022-03-12 03:22:02 -0300

    chore: re index mdlldk

---
+++ main/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Dynamic-link libraries (DLLs) Development Kit for mIRC.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-mdlldk-main.flake = false;
+  inputs.src-mdlldk-main.owner = "rockcavera";
+  inputs.src-mdlldk-main.ref   = "refs/heads/main";
+  inputs.src-mdlldk-main.repo  = "nim-mdlldk";
+  inputs.src-mdlldk-main.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-mdlldk-main"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-mdlldk-main";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
